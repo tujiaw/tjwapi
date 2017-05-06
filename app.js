20aa1f2c5d013d6a0eb61fd6603632b916c20096
tujiaw <tujiaw@163.com>
2017-05-06 21:06:51 +0800

    add

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var base64 = require('./routes/base64');
+var base64img = require('./routes/base64img');
 var api = require('./routes/api');
 
 var app = express();
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/base64', base64);
+app.use('/base64img', base64img);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
