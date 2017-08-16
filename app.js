c00b49671f0b189369c35ee28b028c850d0232d6
tujiaw <tujiaw@163.com>
2017-08-16 23:39:54 +0800

    download image

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.all('*', function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Content-Type");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();  
+});  
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,44 @@
 var express = require('express');
 var router = express.Router();
 var Base64 = require('js-base64').Base64;
+var request = require('request')
+var fs = require('fs')
+var path = require('path')
+
+const dirname = 'uploadImages'
+const hostdir = "./public/mito/"
+function mkdirSync(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true;
+    } else {
+        if (mkdirSync(path.dirname(dirname))) {   
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+    return false
+}
+
+function downloadUrl(urlList) {
+    for (const url of urlList) {
+      const first = url.indexOf(dirname)
+      const last = url.lastIndexOf('/')
+      if (first > 0 && last > 0) {
+        const name = url.substr(last + 1)
+        const dir = url.substr(first, last - first)
+        const dstpath = hostdir + dir + '/' + name
+        if (name.length && dir.length && !fs.existsSync(dstpath)) {
+          if (mkdirSync(hostdir + dir)) {
+            console.log(dstpath)
+            request(url).pipe(fs.createWriteStream(dstpath))
+          }
+        }
+      }
+    }
+}
 
 /* GET home page. */
 router.post('/encode_base64', function(req, res, next) {
-  console.log('xxxxxx');
   if (req.body.text) {
     var dstText = Base64.encode(req.body.text);
     res.json({ success: 1, text: dstText });
@@ -24,4 +58,15 @@ router.post('/decode_base64', function(req, res, next) {
     res.json({ success: 0, error: 'is null'})
   }
 });
+
+router.post('/download_images', function(req, res, next) {
+  const json = req.body.json
+  if (json && json.smallList) {
+    downloadUrl(json.smallList)
+    downloadUrl(json.bigList)
+  }
+  res.json({
+    success: 0
+  })
+})
 module.exports = router;
